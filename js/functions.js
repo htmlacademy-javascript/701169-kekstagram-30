a204a4ec2ac61983cf7c6fae808f56e3158111aa
rydary <rychagova.daria@ya.ru>
2023-10-13 22:52:00 +0300

    Update functions.js

--- js/functions.js
+++ js/functions.js
@@ -1,12 +1,12 @@
 // Функция для проверки длины строки. Она принимает строку, которую нужно проверить, и максимальную длину и возвращает true,
 // если строка меньше или равна указанной длине, и false, если строка длиннее.
 
-function countSymbols (line, maxAmount) {
+function checkMaxLenghtLine (line, maxAmount) {
   const symbolsAmount = line.length;
-  return (symbolsAmount > maxAmount) ? 'Внимание, вы превысили допустимое количество символов!' : line;
+  return (symbolsAmount >= maxAmount);
 }
 
-countSymbols ('Hello World!', 5);
+checkMaxLenghtLine ('Hello World!', 5);
 
 //Функция для проверки, является ли строка палиндромом.
 function checkPalindrom (line) {
@@ -17,7 +17,7 @@ function checkPalindrom (line) {
     emptyLine += newLine[i];
     return emptyLine;
   }
-  const result = (newLine === emptyLine) ? 'Вот так палиндром!' : 'Нет, тут все разное';
+  const result = (newLine === emptyLine);
 
   // eslint-disable-next-line no-console
   console.log(result);
