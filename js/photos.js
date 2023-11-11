48e976f53510d0f4667c8e7a773bdcb0d157193a
rydary <rychagova.daria@ya.ru>
2023-11-11 16:59:50 +0300

    Вносит правки

--- js/photos.js
+++ js/photos.js
@@ -1,6 +1,6 @@
-import {names} from './data.js';
-import { comments } from './data.js';
-import { descriptions } from './data.js';
+import {NAMES} from './data.js';
+import { COMMENTS } from './data.js';
+import { DESCRIPTIONS } from './data.js';
 import { createRandomIdFromRangeGenerator } from './utils.js';
 
 const createPhoto = () => {
@@ -8,21 +8,21 @@ const createPhoto = () => {
   const randomId = createRandomIdFromRangeGenerator(1,26);
   const randomUrl = createRandomIdFromRangeGenerator(1,25);
   const randomAvatar = createRandomIdFromRangeGenerator(1,25);
-  const randomDescription = createRandomIdFromRangeGenerator(0, descriptions.length - 1);
+  const randomDescription = createRandomIdFromRangeGenerator(0, DESCRIPTIONS.length - 1);
   const randomLike = createRandomIdFromRangeGenerator(15, 200);
-  const randomComment = createRandomIdFromRangeGenerator(0, comments.length - 1);
-  const randomName = createRandomIdFromRangeGenerator(0, names.length - 1);
+  const randomComment = createRandomIdFromRangeGenerator(0, COMMENTS.length - 1);
+  const randomName = createRandomIdFromRangeGenerator(0, NAMES.length - 1);
 
   return {
     id: (randomId()),
     url: `photos/${[(randomUrl())]}.jpg`,
-    description: descriptions[(randomDescription)()],
+    description: DESCRIPTIONS[(randomDescription)()],
     likes: (randomLike()),
     message: {
       id: (randomId()),
       avatar:`img/avatar-${[(randomAvatar)()]}.svg`,
-      comment:comments[(randomComment)()],
-      name:names[(randomName())]
+      comment:COMMENTS[(randomComment)()],
+      name:NAMES[(randomName())]
     },
   };
 };
